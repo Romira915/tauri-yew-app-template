7e1c6cf5cbb7a1cbc1173775d024fb9bd9d70025
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-17 05:09:08 +0900

    Add tailwindcss

--- src-yew/src/main.rs
+++ src-yew/src/main.rs
@@ -37,8 +37,8 @@ impl Component for Model {
         let link = ctx.link();
         html! {
             <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <button onclick={link.callback(|_| Msg::SubOne)}>{ "-1" }</button>
+                <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ..." onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <button class="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ..." onclick={link.callback(|_| Msg::SubOne)}>{ "-1" }</button>
                 <p>{ if self.value < 10 {
                     self.value.to_string()
                 } else {
